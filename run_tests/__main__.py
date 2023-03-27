580c90d7dc97b565b315ac43fea68000d5c187f5
Kristofer Reyes <kreyes@gmail.com>
2023-03-27 10:35:51 -0400

    Added run function for package use.

--- run_tests/__main__.py
+++ run_tests/__main__.py
@@ -10,17 +10,21 @@ def _load_data(data_id):
     return np.load(local_file)['arr_0']
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("submission_id")
-parser.add_argument("test_id")
-parser.add_argument("data_id")
-parser.add_argument("--precision", default=4)
-args = parser.parse_args()
+def run(submission_id, test_id, data_id, precision):
+    module = importlib.import_module(submission_id)
+    func = getattr(module, test_id)
+    data = _load_data(data_id)
 
-module = importlib.import_module(args.submission_id)
-func = getattr(module, args.test_id)
-data = _load_data(args.data_id)
+    for i in range(len(data)):
+        np.testing.assert_almost_equal(func(*data[i, :-1]), data[i, -1],
+                                       decimal=precision)
 
-for i in range(len(data)):
-    np.testing.assert_almost_equal(func(*data[i, :-1]), data[i, -1],
-                                   decimal=args.precision)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("submission_id")
+    parser.add_argument("test_id")
+    parser.add_argument("data_id")
+    parser.add_argument("--precision", default=4)
+    args = parser.parse_args()
+    run(args.submission_id, args.test_id, args.data_id, args.precision)
