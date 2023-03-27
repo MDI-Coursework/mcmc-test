6e89d1cca288612216c8f341a9646215bfba11bb
Kristofer Reyes <kreyes@gmail.com>
2023-03-27 19:43:45 -0400

    Added generic gen function

--- run_tests/__init__.py
+++ run_tests/__init__.py
@@ -2,6 +2,7 @@ import importlib
 import gdown
 import numpy as np
 
+
 def _load_data(data_id):
     local_file = "%s.npz" % data_id
     gdown.download(id=data_id, output=local_file, quiet=True)
@@ -16,3 +17,19 @@ def run(submission_id, test_id, data_id, precision):
     for i in range(len(data)):
         np.testing.assert_almost_equal(func(*data[i, :-1]), data[i, -1],
                                        decimal=precision)
+
+
+def gen(submission_id, test_id, seed, num_data, prefix, input_dim,
+        low=None, high=None):
+    module = importlib.import_module(submission_id)
+    func = getattr(module, test_id)
+
+    rng = np.random.default_rng(seed=seed)
+    data = np.zeros((num_data, input_dim + 1))
+    data[:, :4] = rng.uniform(low=low, high=high, size=(num_data, 4))
+
+    for i in range(num_data):
+        data[i, -1] = func(*data[i, :-1])
+
+    np.savez('%s_%016d_%04d.npz' % (prefix, seed, num_data), data)
+    return data
