fbd3790a967762db7f94f330480ebb8f6b95708d
Kristofer Reyes <kreyes@gmail.com>
2023-03-29 21:50:48 -0400

    Added pytest to install requirements.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="run_tests",
     version="1.0",
-    install_requires=['numpy', 'gdown'],
+    install_requires=['numpy', 'gdown', 'pytest'],
     packages=['run_tests']
 )
