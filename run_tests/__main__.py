6d50c8bdab97c39ac1370baa6627cd8bc8efe22c
Kristofer Reyes <kreyes@gmail.com>
2023-04-16 18:33:14 -0400

    Added tyype for output_dim and precision command line arguments

--- run_tests/__main__.py
+++ run_tests/__main__.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     parser.add_argument("submission_id")
     parser.add_argument("test_id")
     parser.add_argument("data_id")
-    parser.add_argument("--precision", default=4)
-    parser.add_argument("--output_dim", default=1)
+    parser.add_argument("--precision", default=4, type=int)
+    parser.add_argument("--output_dim", default=1, type=int)
     args = parser.parse_args()
     run(args.submission_id, args.test_id, args.data_id, args.precision,
         output_dim=args.output_dim)
