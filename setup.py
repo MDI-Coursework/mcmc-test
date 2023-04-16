580519103f82bfcb8cf6690967ee52d2735bf785
Kristofer Reyes <kreyes@gmail.com>
2023-04-16 18:28:37 -0400

    Added scipy to install requirements.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="run_tests",
     version="1.0",
-    install_requires=['numpy', 'gdown', 'pytest'],
+    install_requires=['numpy', 'gdown', 'pytest', 'scipy'],
     packages=['run_tests']
 )
